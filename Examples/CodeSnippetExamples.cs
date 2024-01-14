4cf2ea06ad52f523a229555c83d18925beb808ca
Gordan Kranjcic <gordankranjcic@gmail.com>
2024-01-14 21:57:12 +0100

    Add project files.

---
+++ Examples/CodeSnippetExamples.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    public class CodeSnippetExamples
+    {
+        public void talk()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+
+            for (int i = 10 - 1; i >= 0; i--)
+            {
+
+            }
+
+            var names = new List<string>();
+            foreach (var name in names)
+            {
+
+            }
+
+            while (true)
+            {
+
+            }
+
+            do
+            {
+
+            } while (true);
+        }
+    }
+}
---
+++ Examples/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Examples 
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        
+            for (int i = 0; i < 10; i++) { }
+
+            for (int i = 10 - 1; i >= 0; i--)
+            {
+                        
+            }
+
+
+        }
+    }
+}
